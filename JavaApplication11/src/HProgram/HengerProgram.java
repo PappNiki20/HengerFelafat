9041e68571e01608d4905eef075f8ad48b4cd791
PappNiki20 <papp.nikoletta001@gmail.com>
2023-11-22 13:14:18 +0100

    Alapszerkezet

---
+++ JavaApplication11/src/HProgram/HengerProgram.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package HProgram;
+
+/**
+ *
+ * @author PappNikoletta(SZF_20
+ */
+public class HengerProgram {
+
+   
+    public static void main(String[] args) {
+      
+    }
+    
+}
